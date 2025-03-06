269c93ee637f1076397523644a7b9c9799276fa3
MedHK008 <mohammedherak37@gmail.com>
2025-03-06 10:15:58 +0000

    update server port to 3000, enhance logging, and add Dockerfile for containerization

--- index.js
+++ index.js
@@ -3,21 +3,20 @@ import fetch from "node-fetch";
 import cors from "cors";
 
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 const FLICKR_FEED_URL = "https://www.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1";
 
 app.use(cors());
 
 app.get("/top-images", async (req, res) => {
     try {
-        console.log("Fetching images from Flickr");
+        console.log("Fetching images from Flickr...");
         const response = await fetch(FLICKR_FEED_URL);
+        console.log("Flickr response status:", response.status);
         if (!response.ok) {
             throw new Error("Network response was not ok");
         }
         const data = await response.json();
-        console.log(data
-        );
         const images = data.items.map(item => ({
             title: item.title,
             imageUrl: item.media.m,
@@ -25,7 +24,6 @@ app.get("/top-images", async (req, res) => {
             author: item.author,
             published: item.published
         }));
-
         res.json({ images });
     } catch (error) {
         res.status(500).json({ error: "Failed to fetch images" });
@@ -33,5 +31,5 @@ app.get("/top-images", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}/top-images`);
 });
